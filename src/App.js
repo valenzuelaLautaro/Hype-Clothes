6daa65455eb663c76b011963a6f079e997f6c96f
Lautaro Valenzuela <lautarovalenzuela94@gmail.com>
2022-04-09 16:07:15 -0300

    primera-version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Tienda: Hype Clothes</h1>
+      <p>Lautaro Valenzuela</p>
+    </>
+  );
+}
+
+export default App;
