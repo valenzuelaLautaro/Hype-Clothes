a089ec5f06b1ef8da850678eb2f41ac7414f59ea
Lautaro Valenzuela <lautarovalenzuela94@gmail.com>
2022-04-30 11:42:54 -0300

    ItemListContainer

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,30 +1,10 @@
-import { React, useEffect, useState } from 'react'
+import { React,  } from 'react'
 import Item from './Item'
 
-const ItemList = ({clothesData}) => {
-
-    const [clothes, setDogs] = useState([])
-
-    useEffect(() => {
-        const getClothes = new Promise( (resolve, reject) => {
-            setTimeout(() => {
-                resolve(clothesData)
-            }, 2000)
-        })
-
-        getClothes.then( (result) => {
-            console.log('Se completó la promesa.', result)
-            setDogs(result)
-        }).
-        catch((error) => {
-            console.error(error)
-        })
-
-    }, [])
-
+const ItemList = ({ clothesData }) => {
   return (
     <div className="card-group">
-        { clothes.map(cloth => <Item key={cloth.id} name={cloth.name} description={cloth.description} price={cloth.price} pictureUrl={cloth.pictureUrl} />) }
+        { clothesData.map(cloth => <Item key={cloth.id} name={cloth.name} description={cloth.description} price={cloth.price} pictureUrl={cloth.pictureUrl} />) }
     </div>
   )
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,28 @@
-import React, { Children } from 'react'
+import { React, useEffect, useState } from 'react'
 import ItemList from './ItemList'
-import { clothes } from '../data/clothes'
+import { clothes as clothesData } from '../data/clothes'
 
 import './styles/ItemListContainer.css'
 
 const ItemListContainer = ({ greeting }) => {
+    const [clothes, setDogs] = useState([])
+
+    useEffect(() => {
+        const getClothes = new Promise( (resolve, reject) => {
+            setTimeout(() => {
+                resolve(clothesData)
+            }, 2000)
+        })
+
+        getClothes.then( (result) => {
+            console.log('Se completó la promesa.', result)
+            setDogs(result)
+        }).
+        catch((error) => {
+            console.error(error)
+        })
+
+    }, [])
     console.log(greeting)
     return (
         <div className="item-list-container">
